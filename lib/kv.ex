4f1ffc096dea9d5e2bfc5424ab797840f4e226a1
Alexandre Karpov <alexakarpov@gmail.com>
2019-05-29 23:32:46 -0400

    customize the app

--- lib/kv.ex
+++ lib/kv.ex
@@ -2,11 +2,10 @@ defmodule KV do
   @moduledoc """
   Documentation for KV.
   """
-
   use Application
 
   def start(_type, _args) do
-    KV.Supervisor.start_link []
+    KV.Supervisor.start_link(name: KV.Supervisor)
   end
 
 end
